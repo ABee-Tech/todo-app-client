e23b528cafd8f6565fcc16e43b15f86ea0587e5d
Abhishek Raj Pandey <bhishek_rajpandey@yahoo.com>
2021-12-28 20:16:29 +0545

    created today's tasks component

---
+++ src/components/Todos/TodaysTasks.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Loading from "../Loading/Loading";
+import Todo from "./Todo";
+
+import { fetchTodos, deleteTodo } from "../../redux/actions/todos/todoActions";
+import { Heading, HeadingWithAction } from "../../styles/styles";
+import InfoMessage from "../DisplayMessage/InfoMessage";
+
+function TodaysTasks() {
+  const dispatch = useDispatch();
+  const todoslist = useSelector((state) => state.todosList);
+  const { todos, loading } = todoslist;
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+  return (
+    <div className="row">
+      {loading && <Loading />}
+      <HeadingWithAction>
+        <Heading className="uppercase">Today's Tasks</Heading>
+        <Link to="/addtodo" className="uppercase">
+          + Add Todo
+        </Link>
+      </HeadingWithAction>
+      <div className="col">
+        {todos && todos.length ? (
+          todos.map((todo) => {
+            return <Todo key={todo.id} todo={todo} />;
+          })
+        ) : (
+          <InfoMessage message={"No tasks?? Hurray!!"} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default TodaysTasks;
