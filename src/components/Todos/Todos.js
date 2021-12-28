d4db8008741009abe5b8e6bac40a85a9c356b1bd
Abhishek Raj Pandey <bhishek_rajpandey@yahoo.com>
2021-12-28 20:19:36 +0545

    today's tasks implementatiion

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,47 +1,12 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import Todo from "./Todo";
-import { fetchTodos, deleteTodo } from "../../redux/actions/todos/todoActions";
-import Loading from "../Loading/Loading";
+import React from "react";
 
-import { Heading, HeadingWithAction } from "../../styles/styles";
+import Loading from "../Loading/Loading";
+import TodaysTasks from "./TodaysTasks";
 
 const Todos = () => {
-  const dispatch = useDispatch();
-
-  const todoslist = useSelector((state) => state.todosList);
-  const { todos, loading } = todoslist;
-
-  const handlerDeleteTodo = (id) => {
-    dispatch(deleteTodo(id));
-  };
-
-  const isAdmin = useSelector((state) => state?.userLogin?.userInfo?.role);
-
-  useEffect(() => {
-    dispatch(fetchTodos());
-  }, [dispatch]);
   return (
     <div>
-      {loading && <Loading />}
-      <div className="row">
-        <HeadingWithAction>
-          <Heading className="uppercase">Today's Tasks</Heading>
-          <Link to="/addtodo" className="uppercase">
-            + Add Todo
-          </Link>
-        </HeadingWithAction>
-        <div className="col">
-          {todos && todos.length ? (
-            todos.map((todo) => {
-              return <Todo key={todo.id} todo={todo} />;
-            })
-          ) : (
-            <p>No Todos</p>
-          )}
-        </div>
-      </div>
+      <TodaysTasks />
     </div>
   );
 };
