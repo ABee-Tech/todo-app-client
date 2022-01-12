d8e1eb104a745337069b571acb0e02a7b32a7b98
Abhishek Raj Pandey <bhishek_rajpandey@yahoo.com>
2022-01-12 17:54:14 +0545

    changed source on app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
-import Home from "./components/Home/Home";
+import Home from "./screens/Home/Home";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import AddTodo from "./components/Todos/AddTodo";
 import TodoDetail from "./components/Todos/TodoDetail";
+import Modal from "./components/Modal/Modal";
 
 const App = () => {
+  const buttons = [
+    {
+      name: "Add",
+      onClick: () => {
+        console.log("Add");
+      },
+      onRight: true,
+    },
+  ];
   return (
     <>
+      <Modal
+        title="Add Todo"
+        description="This will permanently deactivate your account"
+        buttons={buttons}
+      >
+        <AddTodo />
+      </Modal>
       <BrowserRouter>
         <Navigation>
           <Route exact path="/" component={Home} />
