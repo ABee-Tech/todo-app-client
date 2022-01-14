9641133f5d0ebb8ef0e984222a8bbea81571311b
Abhishek Raj Pandey <bhishek_rajpandey@yahoo.com>
2022-01-14 18:24:17 +0545

    fixed errors

--- src/components/DisplayMessage/InfoMessage.js
+++ src/components/DisplayMessage/InfoMessage.js
@@ -3,7 +3,7 @@ import React from "react";
 const InfoMessage = ({ message }) => {
   return (
     <div
-      class="border border-bluish-300 rounded-lg flex items-center justify-start px-4 py-2 text-gray-400 bg-gray-100 font-bold"
+      className="border border-bluish-300 rounded-lg flex items-center justify-start px-4 py-2 text-gray-400 bg-gray-100 font-bold"
       role="alert"
     >
       {message}
