e56a549b7c326a82774f8101a223c0d7211366fc
Abhishek Raj Pandey <bhishek_rajpandey@yahoo.com>
2022-01-14 18:33:40 +0545

    Added tailwind styled components and updated

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -5,5 +5,11 @@ export const Heading = styled.h1`
   ${tw`text-sm font-bold text-gray-400`}
 `;
 export const HeadingWithAction = styled.div`
-  ${tw`flex justify-between text-sm font-bold text-bluish-400`}
+  ${tw`flex justify-between text-sm font-bold text-bluish-400 mb-2`}
+`;
+export const LinkButton = styled.button`
+  ${tw`text-sm uppercase font-bold text-bluish-500 hover:text-bluish-800 hover:underline`}
+`;
+export const PrimaryButton = styled.button`
+  ${tw`text-white bg-blue-300 px-4 py-2 rounded-lg mr-2`}
 `;
