8912f2aacf518fc7aab2be88d821ee1c27025fd1
Abhishek Raj Pandey <bhishek_rajpandey@yahoo.com>
2022-01-15 16:43:31 +0545

    to form component and input and button component implement

--- src/components/Todos/AddTodo.js
+++ src/components/Todos/AddTodo.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { createTodo } from "../../redux/actions/todo.actions";
 import { useDispatch, useSelector } from "react-redux";
+import InputWithLabel from "../InputWithLabel/InputWithLabel";
+import { PrimaryButton } from "../../styles/styles";
 
 const AddTodo = ({ history }) => {
   const [title, setTitle] = useState("");
@@ -24,42 +26,29 @@ const AddTodo = ({ history }) => {
     dispatch(createTodo(data));
     history.push("/");
   };
+
   return (
-    <div className="container">
-      <div className="">
-        <h1 className="text-center">Add Todo</h1>
-        <form onSubmit={formSubmitHandler}>
-          <fieldset>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Title </label>
-              <input
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                type="text"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Title"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputPassword1">Description</label>
-              <input
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                type="text"
-                className="form-control"
-                id="exampleInputPassword1"
-                placeholder="Todo description"
-              />
-            </div>
-            <button type="submit" className="btn btn-warning m-auto">
-              Create Todo
-            </button>
-          </fieldset>
-        </form>
-      </div>
-    </div>
+    <form onSubmit={formSubmitHandler}>
+      <fieldset>
+        <InputWithLabel
+          label="Title"
+          value={title}
+          onChangeText={setTitle}
+          type="text"
+          id="title"
+          placeholder="Title"
+        />
+        <InputWithLabel
+          label="Description"
+          value={description}
+          onChangeText={setDescription}
+          type="text"
+          id="description"
+          placeholder="Description"
+        />
+      </fieldset>
+      <PrimaryButton onClick={formSubmitHandler}>Add</PrimaryButton>
+    </form>
   );
 };
 
