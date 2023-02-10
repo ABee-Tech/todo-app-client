efebf94e552c7e4c8b83b0e18d10bb1cb6702502
Abhishek Raj Pandey <bhishek_rajpandey@yahoo.com>
2023-02-10 21:27:03 +0545

    added heading without action component

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,18 +1,26 @@
-import tw from 'twin.macro';
-import styled from 'styled-components';
+import tw from "twin.macro";
+import styled from "styled-components";
 
 export const Heading = styled.h1`
   ${tw`text-sm font-bold text-gray-400`}
 `;
+
 export const HeadingWithAction = styled.div`
   ${tw`flex justify-between text-sm font-bold text-bluish-400 mb-2`}
 `;
+
+export const HeadingWithoutAction = styled.div`
+  ${tw`text-sm font-bold text-bluish-400 mb-2`}
+`;
+
 export const LinkButton = styled.button`
   ${tw`text-sm uppercase font-bold text-bluish-500 hover:text-bluish-800 hover:underline`}
 `;
+
 export const PrimaryButton = styled.button`
   ${tw`text-white bg-blue-400 hocus:bg-blue-500 px-4 py-2 rounded-lg mr-2 h-12`}
 `;
+
 export const SecondaryButton = styled.button`
   ${tw`text-white bg-green-400 hocus:bg-green-500 px-4 py-2 rounded-lg mr-2 h-12`}
 `;
